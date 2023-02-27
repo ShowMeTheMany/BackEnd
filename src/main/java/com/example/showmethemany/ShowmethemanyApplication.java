f0b95ca9b146d0574756db952b903fba3cb63765
code0613 <vhxmfltm3000@naver.com>
2023-02-27 23:56:42 +0900

    [FEAT] 프로젝트 셋팅

---
+++ src/main/java/com/example/showmethemany/ShowmethemanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.showmethemany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowmethemanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowmethemanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/showmethemany/ShowmethemanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.showmethemany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowmethemanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
