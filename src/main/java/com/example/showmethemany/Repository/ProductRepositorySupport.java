d4b5fa1762e8f6773fd09e725cb63ae43a2c5f88
littlezero48 <littlezero.048@gmail.com>
2023-03-08 21:55:32 +0900

    [FEAT] QueryDSL Support 상속 구현 적용

---
+++ src/main/java/com/example/showmethemany/Repository/ProductRepositorySupport.java
@@ -0,0 +1,26 @@
+package com.example.showmethemany.Repository;
+
+import com.example.showmethemany.domain.Products;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.example.showmethemany.domain.QProducts.products;
+
+@Repository
+public class ProductRepositorySupport extends QuerydslRepositorySupport {
+
+    private final JPAQueryFactory queryFactory;
+    public ProductRepositorySupport(Class<?> domainClass, JPAQueryFactory queryFactory) {
+        super(Products.class);
+        this.queryFactory = queryFactory;
+    }
+
+    public List<Products> findByAll () {
+        return queryFactory.select(products)
+                .from(products)
+                .fetch();
+    }
+}
